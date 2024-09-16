656f72f03f05534e1c307715f1ee048024b84d32
WRyan <wesley.ryan03@gmail.com>
2024-09-16 15:33:09 -0300

    segunda aula

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Button from './components/button/button'
+import Card from './components/card/card';
 
 function App() {
   return (
-   <Button></Button>
+   <>
+   <Card />
+   <Card />
+   <Card />
+   </>
   );
 }
 
