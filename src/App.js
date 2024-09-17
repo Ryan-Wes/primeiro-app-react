32f2168a85599de661136577d97cb68e554987a0
WRyan <wesley.ryan03@gmail.com>
2024-09-17 10:51:17 -0300

    Att

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import Button from './components/button/button'
+// import Button from './components/button/button'
 import Card from './components/card/card';
 
 function App() {
   return (
    <>
-   <Card />
-   <Card />
-   <Card />
+    <Card>
+      <h3>Título card 1</h3>
+      <p>esse é um texto do card 1</p>
+    </Card>
+
+    <Card>
+      <h3>Título card 2</h3>
+      <p>esse é um texto do card 2</p>
+    </Card>
+
+    <Card>
+      <h3>Título card 3</h3>
+      <p>esse é um texto do card 3</p>
+    </Card>
    </>
   );
 }
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import './card.css'
 
-class Card extends React.Component {
-    render() {
-        return (
-            <div className='card'>
-                <h3>Título</h3>
-                <p>esse é um texto do card</p>
-            </div>
-        )
-    }
+const Card = ({children}) => {
+    return (
+        <div className='card'>
+            {children}
+        </div>
+    )
 }
+
+// class Card extends React.Component {
+//     render() {
+//         return (
+//             <div className='card'>
+//                 <h3>{this.props.title}</h3>
+//                 <p>esse é um texto do card</p>
+//             </div>
+//         )
+//     }
+// }
 
 export default Card
