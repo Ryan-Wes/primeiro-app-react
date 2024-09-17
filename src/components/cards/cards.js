9f1ce4219499666d71a8ba811eb0e30f9912548d
WRyan <wesley.ryan03@gmail.com>
2024-09-17 13:21:41 -0300

    att5

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -2,6 +2,11 @@ import Card from '../card/card'
 
 const cardsTitle = ['Título card 1', 'Título card 2', 'Título card 3']
 
+const showCardColor = (color) => {
+    console.log(color);
+    
+}
+
 const Cards = () => {
     return (
         <div>
@@ -12,13 +17,18 @@ const Cards = () => {
 
                     cardsTitle.map((cardTitle, index) =>(
                     
-                        <Card key={index}>
+                        <Card key={index} showCardColor={showCardColor}>
                             <h3>{cardTitle}</h3>
                             <p>esse é um texto do card 1</p>
                         </Card>
                     )
                     )
                 }
+
+                        <Card color='blue'showCardColor={showCardColor}>
+                            <h3>Card com fundo azul</h3>
+                            <p>esse é um texto do card 1</p>
+                        </Card>
             </div>
 
         </div>
