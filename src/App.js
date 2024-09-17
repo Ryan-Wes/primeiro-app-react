61de3cf67813839633a7d29d6518f48199bae2c0
WRyan <wesley.ryan03@gmail.com>
2024-09-17 11:19:20 -0300

    Att3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import Button from './components/button/button'
-import Card from './components/card/card';
+// import Button from './components/button/button'
+// import Card from './components/card/card';
+import Cards from './components/cards/cards'
 
 function App() {
   return (
    <>
-    <Button label='Baixar CV'/>
+    {/* <Button label='Baixar CV'/>
 
-    <Button label='Baixar CV'/>
+    <Button /> */}
+
+    <Cards></Cards>
    </>
   );
 }
